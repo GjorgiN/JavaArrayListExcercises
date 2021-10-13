9dc2b08d9a393865f29b97014b909aa86805c995
GjorgiN <gjorgi.nikolovski@gmail.com>
2021-10-13 17:06:07 +0200

    excercise1

---
+++ Excercise1.java
@@ -0,0 +1,32 @@
+package arrayListExcercises;
+
+import java.util.ArrayList;
+
+public class Excercise1 {
+	
+	// add elements in array list
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> colors = new ArrayList<>();
+		
+		System.out.println(colors);
+		
+		colors.add("red");
+		System.out.println(colors);
+		
+		colors.add("Green");
+		System.out.println(colors);
+
+		colors.add("Orange");
+		System.out.println(colors);
+		
+		colors.add("White");
+		System.out.println(colors);
+		
+		colors.add("Black");
+		System.out.println(colors);
+		
+	}
+	
+}
